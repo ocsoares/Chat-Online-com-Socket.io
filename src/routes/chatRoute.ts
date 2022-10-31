44286e9fb9a0201e2f3a8c792e12dbdf58ca61cc
ocsoares <casoares016@gmail.com>
2022-10-31 17:29:10 -0300

    fix return message chat

--- src/routes/chatRoute.ts
+++ src/routes/chatRoute.ts
@@ -23,7 +23,7 @@ chatRoute.get('/chat', chatController.checkIfEnterChat, chatController.webSocket
 
     // Tive que usar o await assim por causa do reverse(), porque o sort retornou ordenado Errado para o meu uso do HTML !! <<
     // sort -1 = Descendente !!
-    const returnMessagesRoom = (await MessageModel.find({ room }).sort({ createdAt: -1 }).limit(4)).reverse();
+    const returnMessagesRoom = (await MessageModel.find({ room }).sort({ createdAt: -1 }).limit(50)).reverse();
 
     res.render(chatEJS, { username, room, returnMessagesRoom });
 });
